d2b655fde212563c7b78760b9dc400cede22be34
Jakob Persons <jakobep24@gmail.com>
2020-11-16 21:37:14 -0500

    added notes

--- newsletter.rb
+++ newsletter.rb
@@ -28,8 +28,6 @@ ARTICLES = [
 #########################
 
 def calculate_recipients
-  # Using the SUBSCRIBERS and UNSUBSCRIBED arrays,
-  # write a method that will return an array of only the subscribers who haven't unsubscribed
   SUBSCRIBERS.difference(UNSUBSCRIBED)
 end
 
@@ -37,18 +35,13 @@ def first_n_articles(number_of_articles)
   ARTICLES.first(number_of_articles)
 end
 
+# * ", " --> converts array into one string seperated by ", "
 def print_recipients
-  # Write a method that uses the output of calculate_recipients
-  # and returns a list of emails separated by commas
-  # Ex) "abc@email.com, def@email.com, ghi@email.com"
   calculate_recipients * ", "
 end
 
 
 def print_one_article(article)
-  # Write a method that will take an article hash
-  # and print the title, author and text as a formatted string
-  # See the README/sample output for examples
   puts "#{article[:title]}\n"
   puts "by: #{article[:author]}\n"
   puts "#{article[:text]}\n"
@@ -56,8 +49,6 @@ end
 
 
 def print_many_articles(articles)
-  # Write a method that will take in an array of article hashes
-  # and format each one using the print_one_article method
   articles.each do |article|
     print_one_article(article)
   end
@@ -95,11 +86,7 @@ def print_newsletter(number)
 end
 
 def run
-  # We want our program to print three articles by default,
-  # but we can change that number here
   print_newsletter(3)
 end
 
-# When we run "ruby newsletter.rb" in the command line,
-# the 'run' method will be called because we're calling it below.
 run
